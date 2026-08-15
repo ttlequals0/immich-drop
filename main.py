a34905a9229d9f36b386393c25055f9a47b415a7
buildplan <hello@buildplan.org>
2026-08-15 09:06:51 +0100

    feat: add proxy support to uvicorn and update docker configuration files

--- main.py
+++ main.py
@@ -13,4 +13,11 @@ if __name__ == "__main__":
         pass
     host = os.getenv("HOST", "0.0.0.0")
     port = int(os.getenv("PORT", "8080"))
-    uvicorn.run("app.app:app", host=host, port=port, reload=True)
+    
+    kwargs = {"host": host, "port": port, "reload": True}
+    trusted_proxy = os.getenv("TRUSTED_PROXY")
+    if trusted_proxy:
+        kwargs["proxy_headers"] = True
+        kwargs["forwarded_allow_ips"] = trusted_proxy
+        
+    uvicorn.run("app.app:app", **kwargs)
